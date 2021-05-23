39b5353abb37c3390c1d3c1f38cd7a28f97fed3b
tuyenld <ninhthangnb@gmail.com>
2021-05-23 21:04:33 +0700

    Edit install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 # !/bin/bash
-gcc -o main main.c bt/lib/libbt.a `pkg-config --cflags gtk+-3.0` `pkg-config --libs gtk+-3.0` -rdynamic -g -Ibt/inc -w
+gcc -o main main.c `pkg-config --cflags --libs gtk+-3.0` -rdynamic -w
