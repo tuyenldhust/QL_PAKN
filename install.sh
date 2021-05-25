a687144654d91a45956b99dabd53b1e18164b000
tuyenld <ninhthangnb@gmail.com>
2021-05-25 23:27:13 +0700

    Update

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 # !/bin/bash
 sudo apt install libgtk-3-dev libssl-dev
 gcc -DSMTP_OPENSSL smtp.c -c -o smtp.o
-gcc -o main main.c smtp.o `pkg-config --cflags --libs gtk+-3.0` -rdynamic -w -lssl -lcrypto
+gcc -o main main.c smtp.o `pkg-config --cflags --libs gtk+-3.0` -rdynamic -w -lssl -lcrypto -DSMTP_OPENSSL
+./main
